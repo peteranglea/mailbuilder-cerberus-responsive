b75e59d53d5e49a6eeee347bd0561cbf19a8a9d0
Peter Anglea <peter@peteranglea.com>
2016-02-11 16:27:48 -0500

    adding simple markdown support for paragraph helper

--- helpers.js
+++ helpers.js
@@ -1,5 +1,9 @@
 Handlebars.registerHelper('mailbuilder-cerberus-responsive-paragraph', function(data) {
   var returnValue = data.replace(/\n/g, '<br />');
   var returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
+  var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+  var returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
+  var returnValue = returnValue.replace(/\*\*([^\s\*][^\*]+[^\s\*])\*\*/, '<b>$1</b>');
+  var returnValue = returnValue.replace(/\*([^\s\*][^\*]+[^\s\*])\*/, '<i>$1</i>');
   return returnValue;
 });
